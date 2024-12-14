53f395c8f42a0268bc072f677500bd417f01c3be
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-12-14 18:01:51 +0300

    starting mysql mastery

---
+++ 18 kasım kod dersi.sql
@@ -0,0 +1,24 @@
+﻿-- Order By -> Sıralıyor
+-- DESC koyarsan tersten sıralıyor
+SELECT * FROM Products Order By UnitPrice DESC;
+
+-- CompanyName Fr ile başlayanları getir
+SELECT* FROM Customers WHERE CompanyName LIKE 'Fr%';
+
+-- CompanyName in içinde Fr olanları getir
+SELECT* FROM Customers WHERE CompanyName LIKE '%Fr%';
+
+SELECT * FROM Customers WHERE City LIKE 'on%'; -- on ile başlayanları getir
+SELECT * FROM Customers WHERE City LIKE '%on'; -- on ile bitenleri getir
+SELECT * FROM Customers WHERE City LIKE '%on%'; -- içinde on olanları getir
+
+SELECT * FROM Products WHERE ProductName = 'Chai';
+
+SELECT * FROM Products WHERE SupplierID = 1 and CategoryID = 6; -- SupplierID 1 VE CategoryID 6 olsun
+SELECT * FROM Products WHERE SupplierID = 1 or CategoryID = 6; -- SupplierID 1 YA DA CategoryID 6 olsun
+
+SELECT * FROM Products Order By UnitsInStock; -- UnitsInStock'a göre sıraladım (azalandan artana)
+
+SELECT MIN(UnitsOnOrder) AS MaksimumSiparis FROM Products;
+SELECT MAX(UnitsOnOrder) AS MinimumSiparis FROM Products;
+SELECT AVG(UnitsOnOrder) AS SiparisOrtalamasi FROM Products;
---
+++ 25 kasım dersi.sql
@@ -0,0 +1,35 @@
+﻿SELECT * FROM Products INNER JOIN Categories on Products.CategoryID=Categories.CategoryID 
+WHERE Products.UnitsInStock < 10;
+
+-- Order Details ve Products tablolarını birleştir, 
+-- 2 satırın birleşme koşulu ikisinin de ProductID si eşitse o satırları birleştir
+-- Order Details (od)'nin unit price'ları 50den büyükse onları al ve birleştir
+-- Şu an küçükten büyüğe sıralar, DESC yazsaydım büyükten küçüğe sıralıyor
+SELECT * FROM [Order Details] od INNER JOIN Products p on od.ProductID=p.ProductID 
+WHERE od.UnitPrice>50 ORDER BY od.UnitPrice;
+
+
+-- Order Details tablosu yerine artık od yazıcam, kısaltma
+-- Categories c yazsaydım Categories.CategoryID yerine c.CategoryID yazacaktım
+
+
+
+SELECT OrderID, p.ProductID, ProductName, CategoryName FROM [Order Details] od 
+INNER JOIN Products p on od.ProductID=p.ProductID 
+INNER JOIN Categories on Categories.CategoryID = p.CategoryID;
+
+SELECT OrderID, LastName FROM Employees e
+INNER JOIN Orders o on e.EmployeeID = o.EmployeeID;
+
+SELECT * FROM [Order Details] od RIGHT JOIN Products p on od.ProductID = p.ProductID
+WHERE od.ProductID is NULL
+
+-- Hiçbir müşteri sipariş versin istemiyoruz, Customers null olacak joinin sağında kaldığı için
+-- Right -> sağ olduğu için Right join
+SELECT * FROM Orders o RIGHT JOIN Customers c on o.CustomerID = c.CustomerID WHERE o.CustomerID is NULL
+
+
+-- Customers bu kez solda kaldığı için LEFT JOIN
+SELECT * FROM Customers c LEFT JOIN Orders o on o.CustomerID = c.CustomerID WHERE o.CustomerID is NULL
+
+SELECT * FROM Employees e LEFT JOIN Orders o on o.EmployeeID = o.EmployeeID WHERE o.EmployeeID IS NULL
